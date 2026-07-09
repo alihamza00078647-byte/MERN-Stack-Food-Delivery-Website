63e71fac68cf4c3814b432f19d96bd90b63cfa69
alihamza00078647-byte <alihamza00078647@gmail.com>
2026-07-09 09:57:04 +0500

    App Changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/order" element={<PlaceOrder />} />
+        
 
       </Routes>
     </div>
